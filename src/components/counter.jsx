aee78e308dd984e9d4918a6fef0b2febc2f03de9
lloyd nzenze <lmooto25@gmail.com>
2018-12-20 16:33:07 +0200

    time to commit fellas

---
+++ src/components/counter.jsx
@@ -0,0 +1,56 @@
+import React, { Component } from "react";
+
+class Counter extends Component {
+  /*constructor() {
+    super();
+    this.handleIncrement = this.handleIncrement.bind(this);
+  }*/
+
+  /*renderTags() {
+    if (this.state.tags.length == 0) return <p>There are no tags!</p>;
+    return (
+      <ul>
+        {this.state.tags.map(tag => (
+          <li key={tag}>{tag}</li>
+        ))}
+      </ul>
+    );
+  }*/
+
+  render() {
+    return (
+      <div>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button
+          onClick={() => this.props.onIncrement(this.props.counter)}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
+        <button
+          onClick={() => this.props.onDelete(this.props.counter.id)}
+          className="btn btn-danger btn-sm m-2"
+        >
+          Delete
+        </button>
+      </div>
+    );
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    if (this.props.counter.value === 0) {
+      classes += "warning";
+    } else {
+      classes += "primary";
+    }
+    return classes;
+  }
+
+  formatCount() {
+    const { value } = this.props.counter;
+    return value === 0 ? "Zero" : value;
+  }
+}
+
+export default Counter;
